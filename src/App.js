47c14c010bdb59b8c7a1597b0b48d18bd32f66ca
RachelSparto <rmbachman2016@hotmail.com>
2022-06-20 12:35:41 -0400

    page ideas and list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import "./App.css";
 
 function App() {
-  return <h1>Legacy Condominium HOA</h1>;
-  <p></p>;
+  return (
+    <>
+      <h1>Legacy Condominium HOA</h1>
+      <hr></hr>
+      <p> nav bar containing</p>
+      <ul>
+        <li>About - which will contain information about the board members</li>
+        <li>Documents - talk to eric and board to find important documents</li>
+        <li>
+          Residents - this should be done and taken care of needing a roster of
+          all people in the community addresses ect.
+        </li>
+        <li>
+          Events - display all community events needs to be able to be updated
+        </li>
+      </ul>
+      <hr></hr>
+      <p>Other things that would be nice</p>
+      <ul>
+        <li>easy way to pay the HOA bills online</li>
+        <li>easy way to rent the club house</li>
+      </ul>
+    </>
+  );
 }
 
 export default App;
